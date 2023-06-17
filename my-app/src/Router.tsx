732f8fb4d0fc2be51f234701f2bd2050177c4d47
landlala <land_lala@naver.com>
2023-06-17 13:25:38 +0900

    #4.2

--- my-app/src/Router.tsx
+++ my-app/src/Router.tsx
@@ -1,18 +1,24 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {createBrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./screens/Home";
 import About from "./screens/About";
+import Root from "./Root";
 
-function Router() {
-    return (
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path = "/" element = {<Home />} />
-                <Route path = "/about" element = {<About />} />
-            </Routes>
-        </BrowserRouter>
-    );
-}
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                path: "about",
+                element: <About />
+            },
+            {
+                path: "",
+                element: <Home />
+            }
+        ]
+    },
+]);
 
-export default Router;
+export default router;
