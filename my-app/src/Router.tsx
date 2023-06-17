a4352211f706b849af89f93c049e95d92721d6a9
landlala <land_lala@naver.com>
2023-06-17 13:45:15 +0900

    #4.3

--- my-app/src/Router.tsx
+++ my-app/src/Router.tsx
@@ -1,8 +1,9 @@
-import {createBrowserRouter, Routes, Route} from "react-router-dom";
-import Header from "./components/Header";
+import {createBrowserRouter} from "react-router-dom";
 import Home from "./screens/Home";
 import About from "./screens/About";
 import Root from "./Root";
+import NotFound from "./screens/NotFound";
+import ErrorComponent from "./components/ErrorComponent";
 
 const router = createBrowserRouter([
     {
@@ -10,14 +11,16 @@ const router = createBrowserRouter([
         element: <Root />,
         children: [
             {
-                path: "about",
-                element: <About />
+                path: "",
+                element: <Home />,
+                errorElement: <ErrorComponent />,
             },
             {
-                path: "",
-                element: <Home />
+                path: "about",
+                element: <About />
             }
-        ]
+        ],
+        errorElement: <NotFound />
     },
 ]);
 
